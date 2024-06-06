15ce30e228fd4744e4c4d2de5ebaa941260cfa17
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2024-06-06 18:26:34 +0200

    Create hory.py

---
+++ hory.py
@@ -0,0 +1,7 @@
+data = [
+    {'nazev': 'Praděd', 'vyska': 1491},
+    {'nazev': 'Klínovec', 'vyska': 1244},
+    {'nazev': 'Smrk', 'vyska': 1124},
+    {'nazev': 'Plechý', 'vyska': 1378},
+    {'nazev': 'Sněžka', 'vyska': 1603},
+]
