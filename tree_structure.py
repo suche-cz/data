9e3ce47ea452c5aa1ac5069c745b452cfccc1af7
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2024-06-11 20:16:50 +0200

    Create tree_structure.py

---
+++ tree_structure.py
@@ -0,0 +1,12 @@
+data = {
+    'jmeno': 'Jana',
+    'rodic': {
+        'jmeno': 'Katka',
+        'rodic': {
+            'jmeno': 'Lena',
+            'rodic': {
+                'jmeno': 'Simi'
+            }
+        }
+    }
+}
